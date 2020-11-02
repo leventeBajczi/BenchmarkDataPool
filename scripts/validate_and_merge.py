1f632d96de9d78107a16654832952317f57665f8
Attila Klenik <a.klenik@gmail.com>
2020-11-02 15:00:50 +0100

    Update validation script to allow empty directories
    Signed-off-by: Attila Klenik <a.klenik@gmail.com>

--- scripts/validate_and_merge.py
+++ scripts/validate_and_merge.py
@@ -5,11 +5,17 @@ import sys
 import os.path
 import itertools
 
-# CSV schema data
+# input doc utils
 doc_numbers = range(1, 7)
+
+doc_prides = [f'Pride{num}' for num in doc_numbers]
+doc_senses = [f'Sense{num}' for num in doc_numbers]
+doc_singles = doc_prides + doc_senses
+
 doc_number_combinations = itertools.product(doc_numbers, doc_numbers)
 doc_combinations = [f'Pride{c[0]}_Sense{c[1]}' for c in doc_number_combinations]
 
+# CSV schema constraints
 csv_header = ['type', 'name', 'input_id', 'time', 'team', 'meas_id']
 column_types = {
     'type': str,
@@ -23,7 +29,7 @@ unique_columns = ['team', 'meas_id', 'name', 'input_id', 'type']
 type_col_values = ['QUEUE', 'START', 'STOP']
 name_col_values = [f'Tokenize{num}' for num in range(1, 3)] + [f'Collect{num}' for num in range(1, 3)] + \
                   [f'ComputeScalar{num}' for num in range(1, 4)] + ['ComputeCosine']
-input_id_col_values = [f'Pride{num}' for num in doc_numbers] + [f'Sense{num}' for num in doc_numbers] + doc_combinations
+input_id_col_values = doc_singles + doc_combinations
 meas_id_col_values = doc_combinations
 
 # Util functions
@@ -61,10 +67,10 @@ def check_for_missing_values(data_frame, col):
 dir_to_validate = sys.argv[1] if len(sys.argv) == 2 else './csvs'
 path_pattern = os.path.join(dir_to_validate, '*.csv')
 merged_csv_path = os.path.join('./', 'MERGED.csv')
-valid_data_frames = []
+processed_data_frames = []
 
 for file_path in glob2.glob(path_pattern):
-    print('Processing file: %s' % file_path)
+    print(f'Processing file "{file_path}"')
     df = pandas.read_csv(file_path, header=0, names=csv_header, low_memory=False, dtype=column_types)
     valid = True
 
@@ -76,17 +82,17 @@ for file_path in glob2.glob(path_pattern):
     valid &= check_for_duplicates(df[unique_columns])
 
     if not valid:
-        print('ERROR: Invalid CSV: ' + file_path)
+        print(f'ERROR: Invalid CSV: "{file_path}"')
         exit(1)
 
-    valid_data_frames.append(df)
+    processed_data_frames.append(df)
 
-if len(valid_data_frames) == 0:
-    print('ERROR: No valid CSVs found in directory ' + dir_to_validate)
-    exit(1)
+if len(processed_data_frames) == 0:
+    print(f'WARNING: No CSVs found in directory "{dir_to_validate}" for pattern "{path_pattern}"')
+    exit(0)
 
 print('Merging CSVs...')
-merged_df = pandas.concat(valid_data_frames)
+merged_df = pandas.concat(processed_data_frames)
 
 if not check_for_duplicates(merged_df[unique_columns]):
     exit(1)
@@ -94,5 +100,5 @@ if not check_for_duplicates(merged_df[unique_columns]):
 print(merged_df.pivot_table(index=['team', 'meas_id', 'name', 'input_id'], columns='type', values='time').reset_index())
 
 merged_df.to_csv(merged_csv_path, index=False)
-print('Valid CSVs saved: ' + merged_csv_path)
+print(f'Valid CSVs saved as "{merged_csv_path}"')
 exit(0)
